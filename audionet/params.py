9aec6acd8af0babb1ea35cdd5ec0b7d830c9ceea
dteal <dteal@dteal.org>
2024-11-23 17:38:24 -0800

    Define global sampling parameters.

--- audionet/params.py
+++ audionet/params.py
@@ -1,20 +1,37 @@
 """Global parameters"""
 
+##########################
+# AUDIO SAMPLING CONSTANTS
+##########################
+
+# Input from anywhere (wave files or microphone) will be resampled
+# to the following format before further processing:
+
+# Make a numpy array of 32-bit floats (the amplitude of sound
+# at each point) of length SAMPLE_RATE * SAMPLE_PERIOD, where
+SAMPLE_RATE = 16000 # (Hz) sample rate
+SAMPLE_PERIOD = 1 # (seconds) length of a recorded audio sample
+
+# This numpy array will be used to create a spectrogram/MFCC/whatever.
+
+##########################
+# MODEL CONSTANTS
+##########################
+
 # Architectural constants.
 NUM_FRAMES = 96  # Frames in input mel-spectrogram patch.
 NUM_BANDS = 64  # Frequency bands in input mel-spectrogram patch.
 EMBEDDING_SIZE = 128  # Size of embedding layer.
 
 # Hyperparameters used in feature and example generation.
-SAMPLE_RATE = 16000
 STFT_WINDOW_LENGTH_SECONDS = 0.025
 STFT_HOP_LENGTH_SECONDS = 0.010
 NUM_MEL_BINS = NUM_BANDS
 MEL_MIN_HZ = 125
 MEL_MAX_HZ = 7500
 LOG_OFFSET = 0.01  # Offset used for stabilized log of input mel-spectrogram.
-EXAMPLE_WINDOW_SECONDS = 0.96  # Each example contains 96 10ms frames
-EXAMPLE_HOP_SECONDS = 0.96     # with zero overlap.
+#EXAMPLE_WINDOW_SECONDS = 0.96  # Each example contains 96 10ms frames
+#EXAMPLE_HOP_SECONDS = 0.96     # with zero overlap.
 
 # Parameters used for embedding postprocessing.
 PCA_EIGEN_VECTORS_NAME = 'pca_eigen_vectors'
