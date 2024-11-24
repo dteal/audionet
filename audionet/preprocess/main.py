e484620ee25004ff5b2aaee0f0c5a295f922a0be
drawnwren <drawnwren@gmail.com>
2024-11-23 17:57:16 -0800

    x

--- audionet/preprocess/main.py
+++ audionet/preprocess/main.py
@@ -1,47 +1,16 @@
-import scipy.signal
 import audionet.params as vggish_params
 import audionet.preprocess.mel_features as mel_features
 import numpy as np
 
-def ensure_sample_rate(original_sample_rate, waveform,
-                       desired_sample_rate=16000):
-  """Resample waveform if required."""
-  if original_sample_rate != desired_sample_rate:
-    desired_length = int(round(float(len(waveform)) /
-                               original_sample_rate * desired_sample_rate))
-    waveform = scipy.signal.resample(waveform, desired_length)
-  return desired_sample_rate, waveform
+import soundfile as sf
 
-def zero_crossing(waveform, sample_rate):
-    # Compute the derivative of the waveform.
-    derivative = scipy.signal.lfilter([1], [1, -1], waveform)
-    indices = np.flatnonzero(np.diff(np.sign(derivative)))
-
-    return indices / float(sample_rate)
-
-
-def auto_correlation(waveform, sample_rate):
-    # Compute the auto-correlation of the waveform.
-    autocorrelation = scipy.signal.correlate(waveform, waveform, mode='full')
-    autocorrelation = autocorrelation[len(waveform):]
-
-    return autocorrelation / np.max(np.abs(autocorrelation))
-
-
-try:
-  import soundfile as sf
-
-  def wav_read(wav_file):
+def wav_read(wav_file):
     wav_data, sr = sf.read(wav_file, dtype='int16')
     return wav_data, sr
 
-except ImportError:
-
-  def wav_read(wav_file):
-    raise NotImplementedError('WAV file reading requires soundfile package.')
 
 def waveform_to_examples(data, sample_rate):
-  """Converts audio waveform into an array of examples for VGGish.
+  """Converts audio waveform into an array of examples 
 
   Args:
     data: np.array of either one dimension (mono) or two dimensions
